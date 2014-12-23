0ce68c1eda49b31853a2a8ecac8ca06ff9817972
Charles Shinaver <bshinaver@gmail.com>
2014-12-23 02:18:35 -0500

    Added assignment 3

--- christmas_server.py
+++ christmas_server.py
@@ -8,14 +8,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 # Assignments
 @app.route("/assignments/assign1.html")
 def assign1():
     return render_template('assign1.html')
 
+
 @app.route("/assignments/assign2.html")
 def assign2():
     return render_template('assign2.html')
+
+
+@app.route("/assignments/assign3.html")
+def assign3():
+    return render_template('assign3.html')
 
 if __name__ == "__main__":
     app.run()
